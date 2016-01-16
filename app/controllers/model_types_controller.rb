c6e05a03d5516aa219cd846ca3da3f3e87a37914
Vincent Auriac <vincent.auriac@gmail>
2016-01-16 08:42:31 +0700

    add ModelTypesController

--- app/controllers/model_types_controller.rb
+++ app/controllers/model_types_controller.rb
@@ -1,2 +1,3 @@
 class ModelTypesController < ApplicationController
+
 end
--- spec/controllers/model_types_controller_spec.rb
+++ spec/controllers/model_types_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 describe ModelTypesController do
+  it '#show' do
+    get :index
+    #expect(assigns[:user]).to be_nil
+    expect(response).to be_success
+    expect(response).to render_template :index
+  end
 end
