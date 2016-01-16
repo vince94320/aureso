f505337285e4c0f6d8b1bb7a7941a06d4b0b8955
Vincent Auriac <vincent.auriac@gmail>
2016-01-16 15:38:52 +0700

    spec refactoring

--- spec/controllers/model_controller_spec.rb
+++ spec/controllers/model_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe ModelController do
-  it '#index' do
+  it 'list models' do
     get :index, model_slug: 'serie-1'
     expect(response).to be_success
     expect(response.header['Content-Type']).to include 'application/json'
--- spec/controllers/model_types_controller_spec.rb
+++ spec/controllers/model_types_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe ModelTypesController do
-  it '#update' do
+  it 'updates model type' do
     post :update, model_slug: 'serie-1', model_type_slug: 'bmw-330i', base_price: 150000
     expect(response).to be_success
     expect(response.header['Content-Type']).to include 'application/json'
